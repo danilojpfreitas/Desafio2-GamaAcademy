1ece8fbd20f172404eb55372430fc0a0fa86590e
danilojpfreitas <danilojpfreitas@gmail.com>
2022-07-04 21:01:54 -0300

    Versão 8

--- js/eventos.js
+++ js/eventos.js
@@ -19,25 +19,29 @@ fetch(`https://xp41-soundgarden-api.herokuapp.com/events`, {
               <div class="modal-dialog" role="document">
                  <div class="modal-content">
                      <div class="modal-header">
-                         <p class="modal-title">Digite seus dados para realizar a reserva de ${data[i].name} :)</p>
+                         <p class="modal-title">Digite seus dados para realizar a reserva do evento: ${data[i].name} :)</p>
                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                          <span aria-hidden="true">&times;</span>
                          </button>
                      </div>
                      <div class="modal-body">
-                         <form class="col-6" id="create-event-form">
+                         <form class="col-6" id="create-event-form${data[i]._id}">
                              <div class="mb-3">
                              <label for="nome" class="form-label">Nome</label>
-                             <input type="text" class="form-control" id="nome" aria-describedby="nome"/>
+                             <input type="name" class="form-control" id="nome" aria-describedby="nome"/>
                              </div>
                              <div class="mb-3">
                              <label for="email" class="form-label">Email</label>
-                             <input type="text" class="form-control" id="email" aria-describedby="email"/>
+                             <input type="email" class="form-control" id="email" aria-describedby="email"/>
+                             <br>
+                             <div class="mb-3">
+                             <label for="reservas" class="form-label">Reservas</label>
+                             <input type="number" class="form-control" id="reservas" aria-describedby="reservas"/>
                              </div>
                          </form> 
                      </div>
                      <div class="modal-footer">
-                         <button type="submit" class="btn btn-primary">enviar</button>
+                         <button type="submit" class="btn btn-primary" onclick="criarReserva('${data[i]._id}')">enviar</button>
                      </div>
                     </div>
                  </div>
@@ -59,4 +63,25 @@ fetch(`https://xp41-soundgarden-api.herokuapp.com/events`, {
         console.error(error.message)
     });
 }));
+
+async function criarReserva(idEventoReserva) {
+    const formReserva = document.getElementById(`create-event-form${idEventoReserva}`);
+    formReserva.addEventListener('submit', e => e.preventDefault())
+    const enviarReserva = {
+        owner_name: formReserva.elements[0].value,
+        owner_email: formReserva.elements[1].value,
+        number_tickets: parseInt(formReserva.elements[2].value),
+        event_id: idEventoReserva
+    }
+    console.log(formReserva.elements[0].value)
+    await fetch(`https://xp41-soundgarden-api.herokuapp.com/bookings`, {
+        method: "POST",
+        body: JSON.stringify(enviarReserva),
+        headers: { "Content-type": "application/json; charset=UTF-8" }
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .then(window.alert('Reserva Realizada'))
+    .catch(err => console.log(err))
+}
 
